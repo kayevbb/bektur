4ef62996b9749f4b8feb9c1d18438f2b9ed30fd8
Bektur <tip.beka99@gmail.com>
2019-10-23 11:56:22 +0600

    beka

---
+++ func.py
@@ -0,0 +1,67 @@
+# def some_func():
+#     return "some func"
+# print(some_func())
+#
+# def sum_(num1, nem2=0, some_parametr=Nune):
+#     return num1 + nem2 + some_parametr
+# print(sum_(12,15,13))
+#
+# def save(*args, **kwargs):
+#     print(args[0] + args[1])
+#     print(kwargs.get("name") + kwargs.get("lastname"))
+#     return args, kwargs
+# print(save("hello", "world", name="python",lastname="Zen",))
+
+# def save(*args):
+#     return args
+# print(save(1,2,3,4,5,6,7,))
+
+# def some_func(name, lastname):
+#     print(locals())
+# some_func()
+#
+# name = "Asus"
+# print((locals()))
+# print()
+# print()
+# print()
+# name = "HP"
+# print(locals())
+
+
+# name = "Lenova"
+# def func_one():
+# #   name = 'func_one()'
+#   def func_two():
+#     name = "Acer"
+#     print(name)
+#     print(locals())
+#
+#   func_two()
+#
+# func_one()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
